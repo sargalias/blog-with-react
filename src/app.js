2a8ab5197bb725d26ba21234b43261b846377807
Spyros Argalias <sargalias@gmail.com>
2018-08-25 21:14:40 +0100

    add webpack basic build, add babel

---
+++ src/app.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const app = () => (
+  <div>
+    <h1>Hello</h1>
+  </div>
+);
+
+ReactDOM.render(app, document.getElementById('app'));
---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/app.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'public', 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      }
+    ]
+  }
+};
