dc09aeea061bf82ef63d446bea51b7ac3e46691a
Spyros Argalias <sargalias@gmail.com>
2018-09-08 21:01:45 +0100

    configure jest to mock out styles
    This allows jest to be able to run, even if styles are imported in the project, otherwise it can't execute.

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,9 @@
 module.exports = {
   verbose: true,
   setupTestFrameworkScriptFile: "<rootDir>src/setupTests.js",
-  snapshotSerializers: ["enzyme-to-json/serializer"]
+  snapshotSerializers: ["enzyme-to-json/serializer"],
+  moduleNameMapper: {
+    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/__mocks__/fileMock.js",
+    "\\.(css|sass|scss|less)$": "<rootDir>/__mocks__/styleMock.js"
+  }
 };
