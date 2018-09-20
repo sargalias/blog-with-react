501ca43e55d6730418a7676610b00e5f128c5f7d
Spyros Argalias <sargalias@gmail.com>
2018-09-20 14:23:26 +0100

    Add tests for NavLink for correct prop usage.
    A test which shows that NavLink correctly uses its 'label' prop.
    Two tests which show that NavLink correctly uses its 'isActive' prop.

--- src/tests/NavLink.test.js
+++ src/tests/NavLink.test.js
@@ -16,3 +16,24 @@ test('NavLink should render correctly with props and isActive false', () => {
   const wrapper = shallow(<NavLink link='/test' label='test' isActive={true} />);
   expect(wrapper).toMatchSnapshot();
 });
+
+test('NavLink has correct label', () => {
+  const label = 'test text';
+  const re = new RegExp(label);
+  const wrapper = shallow(<NavLink label={label} />);
+  expect(wrapper.text()).toMatch(re);
+});
+
+test('NavLink is set correctly when isActive=true', () => {
+  const isActive = true;
+  const expected = ' (current)';
+  const wrapper = shallow(<NavLink isActive={isActive} />);
+  expect(wrapper.text()).toBe(expected);
+});
+
+test('NavLink is set correctly when isActive=false', () => {
+  const isActive = false;
+  const expected = ' ';
+  const wrapper = shallow(<NavLink isActive={isActive} />);
+  expect(wrapper.text()).toBe(expected);
+});
