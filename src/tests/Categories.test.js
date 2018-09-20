80ad8d037e36ed1446d6f32d23f90e2301b1c718
Spyros Argalias <sargalias@gmail.com>
2018-09-20 02:21:15 +0100

    Add tests for Categories.
    Snapshot test checks that Categories renders correctly with no props.
    
    More tests which check Categories is calling CategoryButtons with
    correct props.

--- src/tests/Categories.test.js
+++ src/tests/Categories.test.js
@@ -3,6 +3,13 @@ import { shallow } from 'enzyme';
 import Categories from '../components/Categories';
 import CategoryButton from '../components/CategoryButton';
 
+const categories = ['Food', 'Home', 'Travel', 'Sports'];
+
+test('Categories renders correctly with no props', () => {
+  const wrapper = shallow(<Categories />);
+  expect(wrapper).toMatchSnapshot();
+});
+
 test('Categories renders correctly with sample props', () => {
   const wrapper = shallow(<Categories categories={['Food', 'Home', 'Travel']} activeCategory={'Home'} />);
   expect(wrapper).toMatchSnapshot();
@@ -44,5 +51,29 @@ test('Categories with activeCategory creates children CategoryButton with correc
       expect(isActive).toBe(true);
     else
       expect(isActive).toBe(false);
+  });
+});
+
+test('Categories sets up no CategoryButtons with no categories passed in', () => {
+  const wrapper = shallow(<Categories />);
+  expect(wrapper.find('CategoryButton').length).toBe(0);
+});
+
+test('Categories correctly sets up CategoryButtons with categories passed in and no active category', () => {
+  const wrapper = shallow(<Categories categories={categories} />);
+  wrapper.find('CategoryButton').forEach((cb, i) => {
+    const {label, isActive} = cb.props();
+    expect(label).toBe(categories[i]);
+    expect(isActive).toBe(false);
+  });
+});
+
+test('Categories correctly sets up CategoryButtons with categories and active category', () => {
+  const activeCategoryIndex = 2;
+  const wrapper = shallow(<Categories categories={categories} activeCategory={categories[2]} />);
+  wrapper.find('CategoryButton').forEach((cb, i) => {
+    const {label, isActive} = cb.props();
+    expect(label).toBe(categories[i]);
+    expect(isActive).toBe(activeCategoryIndex === i);
   });
 });
