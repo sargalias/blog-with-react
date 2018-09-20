2595d45bcc69a85ed4b3733e3418f01bbd7a5b4c
Spyros Argalias <sargalias@gmail.com>
2018-09-20 12:49:43 +0100

    Delete redundant tests for Categories component.
    The tests removed are tests whose functionality is better encompassed
    by the new tests created in commit 5e4b5a3.

--- src/tests/Categories.test.js
+++ src/tests/Categories.test.js
@@ -15,45 +15,6 @@ test('Categories renders correctly with sample props', () => {
   expect(wrapper).toMatchSnapshot();
 });
 
-test('Categories renders with correct number of CategoryButton children', () => {
-  const categories = ['t1', 't2', 't3'];
-  const wrapper = shallow(<Categories categories={categories} />);
-  expect(wrapper.find(CategoryButton).length).toBe(categories.length);
-});
-
-test('Categories renders with correct CategoryButton names', () => {
-  const categories = ['t1', 't2', 't3'];
-  const wrapper = shallow(<Categories categories={categories} />);
-  const categoryButtons = wrapper.find(CategoryButton);
-  categoryButtons.forEach((categoryButton, i) => {
-    expect(categoryButton.prop('label')).toBe(categories[i]);
-  });
-});
-
-test('Categories with activeCategory=false creates children CategoryButton with isActive=false', () => {
-  const categories = ['t1', 't2', 't3'];
-  const wrapper = shallow(<Categories categories={categories} />);
-  const categoryButtons = wrapper.find(CategoryButton);
-  categoryButtons.forEach(categoryButton => {
-    let { isActive } = categoryButton.props();
-    expect(isActive).toBe(false);
-  });
-});
-
-test('Categories with activeCategory creates children CategoryButton with correct isActive props', () => {
-  const activeCategory = 't3';
-  const categories = ['t1', 't2', 't3'];
-  const wrapper = shallow(<Categories categories={categories} activeCategory={activeCategory} />);
-  const categoryButtons = wrapper.find(CategoryButton);
-  categoryButtons.forEach(categoryButton => {
-    let { label, isActive } = categoryButton.props();
-    if (label === activeCategory)
-      expect(isActive).toBe(true);
-    else
-      expect(isActive).toBe(false);
-  });
-});
-
 test('Categories sets up no CategoryButtons with no categories passed in', () => {
   const wrapper = shallow(<Categories />);
   expect(wrapper.find('CategoryButton').length).toBe(0);
