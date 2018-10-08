2beebc7cf98ed86aa8cf7725bcd2d85d3c8c212d
Spyros Argalias <sargalias@gmail.com>
2018-10-08 06:15:28 +0100

    Add a user to blogPosts fixtures.

--- src/tests/fixtures/blogPosts.js
+++ src/tests/fixtures/blogPosts.js
@@ -1,4 +1,5 @@
 import moment from 'moment';
+import users from './users';
 
 const blogPosts = [
   {
@@ -7,7 +8,8 @@ const blogPosts = [
     title: 'Test title',
     excerpt: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est fugiat harum obcaecati pariatur sint tempore unde. Aperiam commodi distinctio earum eligendi, enim itaque laudantium magni neque porro temporibus vitae voluptatum?',
     postId: '1',
-    date: moment(0)
+    date: moment(0),
+    user: users[0]
   },
   {
     imageURL: 'https://via.placeholder.com/200x200',
@@ -15,7 +17,8 @@ const blogPosts = [
     title: 'Test title 2',
     excerpt: 'Test excerpt 2',
     postId: '2',
-    date: moment(0).add(5, 'days')
+    date: moment(0).add(5, 'days'),
+    user: users[1]
   },
   {
     imageURL: 'https://via.placeholder.com/250x250',
@@ -23,7 +26,8 @@ const blogPosts = [
     title: 'Test title 3',
     excerpt: '3 Lorem ipsum dolor',
     postId: '3',
-    date: moment(0).subtract(5, 'days')
+    date: moment(0).subtract(5, 'days'),
+    user: users[2]
   },
 ];
 
